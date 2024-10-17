1a747e66bc6c9ce76d34149a0d43df4b5afbfc54
nurcin <nurchin.14@gmail.com>
2024-10-17 19:38:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace a3t1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value:");
+            int number ;
+            number = int.Parse(Console.ReadLine());
+
+            int count=0 ;
+
+            if (number == 0 )
+            {
+                count = 1 ;
+            }
+            else if (number <0 ) { 
+            number = -number ;
+            
+            }
+            while (number > 0)
+            {
+                number = number / 10;
+                count++;
+            }
+
+                Console.WriteLine($"Mertebe sayi: {count}");
+
+            
+
+
+        }
+    }
+}
